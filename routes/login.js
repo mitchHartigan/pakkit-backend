55f328b7617b9494f1bbe3e83341584f4cec676f
Mitchell Hartigan <mitch.hartigan@gmail.com>
2021-02-21 23:36:00 -0800

    fix: function not being called

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bodyParser = require("body-parser");
 
-router.use(bodyParser.json);
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
 
 router.post("/", (req, res) => {
   console.log("request recieved:");
