cf9fd9b67a5baa23689a79868a71bf17903c4234
Mitchell Hartigan <mitch.hartigan@gmail.com>
2021-03-18 15:41:47 -0700

    feat: added error messages to login route

--- routes/login.js
+++ routes/login.js
@@ -28,7 +28,7 @@ router.post("/", (req, res) => {
     // If returned data obj is empty, email did not match any records in the db.
     if (Object.keys(data).length === 0) {
       console.log("Email not found in database");
-      res.sendStatus(404);
+      res.status(404).json(`Email not found in database.`);
     } else {
       bcrypt.compare(password, data.Item.password, (err, same) => {
         if (same) {
@@ -37,8 +37,7 @@ router.post("/", (req, res) => {
           const token = jwt.sign(payload, process.env.SECRET_OR_KEY);
           res.status(200).json(token);
         } else {
-          console.log("incorrect password");
-          res.sendStatus(401);
+          res.status(401).json("Password is incorrect");
         }
       });
     }
